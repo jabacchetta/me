e0fbcbe10081a155fb21cc9e48ab1fbe5775ad17
jabacchetta <jabacchetta@gmail.com>
2017-12-03 19:48:21 -0600

    Add responsive background images
    Optimized images added for different screen sizes and resolutions.

--- components/layout/App/styles/base.js
+++ components/layout/App/styles/base.js
@@ -17,7 +17,37 @@ export default `
     position: fixed;
     top: 0;
     left: 0;
-    background: linear-gradient(45deg, rgba(132, 67, 138, 0.31) 21%, rgba(65, 129, 224, 0.188) 100%), url(/static/background@4x.jpg) center / cover, var(--color-primary);
+    background: linear-gradient(45deg, rgba(132, 67, 138, 0.31) 21%, rgba(65, 129, 224, 0.188) 100%), url(/static/background.jpg) center / cover, var(--color-primary);
+  }
+  
+  @media
+  only screen and (min-width: 720px),
+  only screen and (min-resolution: 1.2dppx) and (min-width: 360px),
+  only screen and (min-resolution: 2.2dppx) and (min-width: 240px),
+  only screen and (min-resolution: 3.2dppx) and (min-width: 180px) {
+    body:before {
+      background: linear-gradient(45deg, rgba(132, 67, 138, 0.31) 21%, rgba(65, 129, 224, 0.188) 100%), url(/static/background@2x.jpg) center / cover, var(--color-primary);
+    }
+  }
+  
+  @media
+  only screen and (min-width: 1440px),
+  only screen and (min-resolution: 1.2dppx) and (min-width: 720px),
+  only screen and (min-resolution: 2.2dppx) and (min-width: 480px),
+  only screen and (min-resolution: 3.2dppx) and (min-width: 360px) {
+    body:before {
+      background: linear-gradient(45deg, rgba(132, 67, 138, 0.31) 21%, rgba(65, 129, 224, 0.188) 100%), url(/static/background@3x.jpg) center / cover, var(--color-primary);
+    }
+  }
+  
+  @media
+  only screen and (min-width: 2160px),
+  only screen and (min-resolution: 1.2dppx) and (min-width: 1080px),
+  only screen and (min-resolution: 2.2dppx) and (min-width: 720px),
+  only screen and (min-resolution: 3.2dppx) and (min-width: 540px) {
+    body:before {
+      background: linear-gradient(45deg, rgba(132, 67, 138, 0.31) 21%, rgba(65, 129, 224, 0.188) 100%), url(/static/background@4x.jpg) center / cover, var(--color-primary);
+    }
   }
   
   @media (min-width: 1280px) {
