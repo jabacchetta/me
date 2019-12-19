1bcc9617351c8c8ad6e7fcccd074cf3a179f5221
Jason Bacchetta <jabacchetta@gmail.com>
2019-12-19 01:28:55 -0600

    Update resume and about page

--- components/collections/About/index.js
+++ components/collections/About/index.js
@@ -7,8 +7,8 @@ const About = ({ toggleView }) => ([
   <Text key="title" title>Jason Bacchetta</Text>,
   <Text key="text" content>
     Husband, father, theist, entrepreneur, software engineer, brand architect, technology hound,
-    sports fan, coffee aficionado, cryptocurrency investor, Google fanboy, video game addict, Game
-    of Thrones enthusiast, fantasy football champ, aspiring wordsmith, would-be philosopher.
+    sports fan, coffee aficionado, blockchain investor, video game addict, Game of Thrones
+    enthusiast, fantasy football champ, aspiring wordsmith, would-be philosopher.
   </Text>,
   <Button key="btn1" primary onClick={toggleView}>Contact</Button>,
   <Button key="btn2" onClick={() => window.open(`/static/Resume.pdf`)}>
